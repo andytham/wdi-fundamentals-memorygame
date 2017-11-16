6568b87cc953133dab22a2ad5943b6098bbdef51
andy <thamandyy@gmail.com>
2017-11-16 18:15:26 -0500

    added DOM to game logic

--- js/main.js
+++ js/main.js
@@ -32,24 +32,32 @@ var checkForMatch = function() {
 	
 }
 
-var flipCard = function (cardId){
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
-	cardsInPlay.push(cards[cardId].rank);
+var flipCard = function (){
+	console.log("User flipped " + cards[this.getAttribute('data-id')].rank);
+	console.log(cards[this.getAttribute('data-id')].cardImage);
+	console.log(cards[this.getAttribute('data-id')].suit);
+	cardsInPlay.push(cards[this.getAttribute('data-id')].rank);
+
+	this.setAttribute('src', cards[this.getAttribute('data-id')].cardImage);
 
 	if (cardsInPlay.length === 2){
 		checkForMatch();
 	}
-
-
 }
 
+var createBoard = function(){
+	for (var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
 
+createBoard();
 
-flipCard(0);
-flipCard(2);
-
-
+//add reset button
+//add score tracker
 
 console.log("Up and running!")
